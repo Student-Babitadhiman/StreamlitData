1a09f36fc95e641f0b5087c3475b04e951ff373b
Student-Babitadhiman <babitadhiman746@gmail.com>
2025-03-31 10:01:38 +0530

    __init__.py files added

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from .ui_components import *
+from .request_handling import *
+from .auth_components import *
+
+from app import render_request_form, submit_request
---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+from .snowflake_connection import connector_connection, session
+
+from config import connector_connection
+
